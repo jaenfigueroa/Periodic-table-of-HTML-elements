de2a4f174fcbe7d4bece735b5af6c8187cd1840d
jaen <jaenfigueroa03042000@gmail.com>
2023-05-28 17:39:53 -0500

    :bug: fix: endpoint json to elements

--- src/helpers/getElements.ts
+++ src/helpers/getElements.ts
@@ -1,9 +1,13 @@
 export const getElements = async () => {
+  // const URL = '/data/elements.json'
+  const URL = 'https://raw.githubusercontent.com/jaenfigueroa/Periodic-table-of-HTML-elements/main/data/elements.json'
+
   try {
-    const response = await fetch('../../data/elements.json')
+    const response = await fetch(URL)
     const elements = await response.json()
     return elements
   } catch (error) {
     console.log(error)
   }
 }
+
