2c5a5e8723aef60bf047cf6145b4d2b59d2a8187
jaen <jaenfigueroa03042000@gmail.com>
2023-05-30 02:47:53 -0500

    :sparkles: feat: reorder elements

--- src/components/atoms/Element/Element.tsx
+++ src/components/atoms/Element/Element.tsx
@@ -10,26 +10,23 @@ export const Element = ({ element }: Props) => {
   const { tag, group, url } = element
 
   const buttonClasses = classnames('element', {
-    'element--green': group === 'Root element',
-    'element--blue': group === 'Elementos de estructura',
-    'element--violet': group === 'Elementos multimedia',
-    // 'element--yellow': group === 'Text-level semantics',
-    'element--orange': group === 'Elementos de lista',
-    'element--green2': group === 'Elementos de formulario',
-    'element--blue2': group === 'Document sections',
-    'element--orange2': group === 'Elementos de tabla',
-    'element--gray': group === 'Elementos de interacción',
-
-    'element--white': group === 'Contenido incrustado',
-
-    'element--yellow': group === 'Elementos de texto',
-    'element--green3': group === 'Elementos de enlace',
-    'element--skyblue': group === 'Elementos de enfasis',
-    'element--morado': group === 'Estilo y presentacion',
-    'element--orange3': group === 'Codigo y variable',
-    'element--pink': group === 'Elementos de anotaciones',
-    'element--yellow2': group === 'Elementos de direccion',
-    'element--green4': group === 'Elementos de salto de linea',
+    'element--green': group === 'Elemento root',
+    'element--blue': group === 'Elems. de estructura',
+    'element--violet': group === 'Elems. multimedia',
+    'element--orange': group === 'Elems. de lista',
+    'element--green2': group === 'Elems. de formulario',
+    'element--blue2': group === 'Elems. para secciones',
+    'element--orange2': group === 'Elems. de tabla',
+    'element--gray': group === 'Elems. de interacción',
+    'element--white': group === 'Elems. de contenido incrustado',
+    'element--yellow': group === 'Elems. de texto',
+    'element--green3': group === 'Elems. de enlace',
+    'element--skyblue': group === 'Elems. de enfasis',
+    'element--morado': group === 'Elems. de estilo y presentacion',
+    'element--orange3': group === 'Elems. de codigo y variable',
+    'element--pink': group === 'Elems. de anotaciones',
+    'element--yellow2': group === 'Elems. de direccion',
+    'element--green4': group === 'Elems. de salto de linea',
   })
 
   return (
