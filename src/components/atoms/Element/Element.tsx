ee7e599c1dac6a46640c383a0c900222db9d5b29
jaen <jaenfigueroa03042000@gmail.com>
2023-05-29 20:15:11 -0500

    :sparkles: feat: add more colors and html elements

--- src/components/atoms/Element/Element.tsx
+++ src/components/atoms/Element/Element.tsx
@@ -11,14 +11,25 @@ export const Element = ({ element }: Props) => {
 
   const buttonClasses = classnames('element', {
     'element--green': group === 'Root element',
-    'element--blue': group === 'Metadata and scripting',
-    'element--violet': group === 'Embedding content',
-    'element--yellow': group === 'Text-level semantics',
-    'element--orange': group === 'Grouping content',
-    'element--green2': group === 'Forms',
+    'element--blue': group === 'Elementos de estructura',
+    'element--violet': group === 'Elementos multimedia',
+    // 'element--yellow': group === 'Text-level semantics',
+    'element--orange': group === 'Elementos de lista',
+    'element--green2': group === 'Elementos de formulario',
     'element--blue2': group === 'Document sections',
-    'element--orange2': group === 'Tabular data',
-    'element--gray': group === 'Interactive elements',
+    'element--orange2': group === 'Elementos de tabla',
+    'element--gray': group === 'Elementos de interacción',
+
+    'element--white': group === 'Contenido incrustado',
+
+    'element--yellow': group === 'Elementos de texto',
+    'element--green3': group === 'Elementos de enlace',
+    'element--skyblue': group === 'Elementos de enfasis',
+    'element--morado': group === 'Estilo y presentacion',
+    'element--orange3': group === 'Codigo y variable',
+    'element--pink': group === 'Elementos de anotaciones',
+    'element--yellow2': group === 'Elementos de direccion',
+    'element--green4': group === 'Elementos de salto de linea',
   })
 
   return (
