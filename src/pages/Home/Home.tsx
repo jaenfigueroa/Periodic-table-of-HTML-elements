3d83416fa5482968beb7ed3488b3b40988c0b032
jaen <jaenfigueroa03042000@gmail.com>
2023-06-11 13:13:15 -0500

    :sparkles: feat: add description to multimedia elements

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,6 +12,16 @@ export const Home = () => {
       <Main>
         <div className='table-container'>
           <Table periodicContent={elementsData2.elements} />
+          <details>
+            <summary>Ver tambien los elementos obsoletos o deprecados</summary>
+            <div>
+              <p>item1</p>
+              <p>item1</p>
+              <p>item1</p>
+              <p>item1</p>
+              <p>item1</p>
+            </div>
+          </details>
           <Distribution distributionContent={elementsData2.distribution} />
         </div>
       </Main>
